8008df2414885a3c06cbd12fd64c8cf34cfb0f95
Robbie Bardijn <robbie.bardijn@crosscheck.be>
2016-03-17 11:37:12 +0100

    master: more code :D

---
+++ index.js
@@ -0,0 +1,161 @@
+#!/usr/bin/env node
+
+var inquirer = require('inquirer');
+var simpleGit = require('simple-git')( process.cwd() );
+
+var fs = require('fs');
+var ejs = require('ejs');
+var templateString = fs.readFileSync(__dirname + '/template.ejs', 'utf-8');
+var chalk = require('chalk');
+var ncp = require('copy-paste');
+
+var branch = '';
+var ticket = '';
+var project = '';
+
+var getCodereview = function(answers) {
+  simpleGit.getRemotes(true, function(err, res) {
+    if(!err) {
+      console.log(res);
+    }
+  });
+};
+
+var askQuestions = function(branch) {
+  var questions = [];
+  questions.push({
+    type: 'input',
+    name: 'ticket',
+    message: function() {
+      if(branch !== '?') {
+        return 'TICKET: Please enter your ticket. Maybe it is?';
+      } else {
+        return 'TICKET: Please enter your ticket.';
+      }
+    },
+    default: branch,
+    validate: function (value) {
+      if(value === '' || value === '?') {
+        return 'You must enter a ticket.';
+      }
+
+      if (value.match(/^(master|acc|staging|prod)$/)) {
+        return 'You entered an enviroment branch for the ticket, this is not going to work :)';
+      }
+
+      if (value.indexOf('issue') > -1) {
+        return 'You should not use the prefix issue in your ticketname, this is not going to work :)';
+      }
+
+
+      return true;
+    }
+  });
+
+  questions.push({
+    type: 'input',
+    name: 'info',
+    message: 'GENERAL INFO: Please provide the information needed for your colleagues.',
+    default: 'The issue was solved'
+  });
+
+  questions.push({
+    type: 'checkbox',
+      name: 'git',
+      message: 'GIT: In what branches was your issue merged?',
+      choices: [{
+        name: 'master',
+        value: 'master',
+        checked: true
+      }, {
+        name: 'dev',
+        value: 'dev',
+        checked: true
+      }, {
+        name: 'acc',
+        value: 'acc',
+        checked: true
+      }, {
+        name: 'staging',
+        value: 'staging',
+        checked: false
+      }, {
+        name: 'prod',
+        value: 'prod',
+        checked: false
+      }]
+    });
+
+  questions.push({
+    type: 'checkbox',
+      name: 'deploy',
+      message: 'DEPLOY: On what envs was your issue deployed?',
+      choices: [{
+        name: 'development',
+        value: 'Development',
+        checked: true
+      }, {
+        name: 'acceptation',
+        value: 'Acceptation(QA)',
+        checked: true
+      }, {
+        name: 'staging',
+        value: 'Staging',
+        checked: false
+      }, {
+        name: 'production',
+        value: 'Production',
+        checked: false
+      }]
+    });
+
+  questions.push({
+    type: 'input',
+    name: 'screenshot',
+    message: 'SCREENSHOT: You can add additional screenshots of the solution here.'
+  });
+
+  questions.push({
+    type: 'input',
+    name: 'url',
+    message: 'URL: Please be kind for QA and add a test url.'
+  });
+
+  inquirer.prompt(questions, function (answers) {
+    console.log(answers);
+    getCodereview(answers);
+    console.log(answers);
+    // console.log(chalk.green(ejs.render(templateString, answers)));
+  });
+};
+
+if (fs.existsSync(process.cwd() + '/.git')) {
+  simpleGit.revparse(['--abbrev-ref', 'HEAD'], function(err, res) {
+    if(!err) {
+      res = res.replace(/(\r\n|\n|\r)/gm,'');
+
+      branch = res;
+      if(branch.indexOf('issue-') > -1) {
+        ticket = branch.substr(6);
+      }
+
+      askQuestions(ticket);
+    }
+  });
+} else {
+  console.log(chalk.black.bgYellow('WARNING') + chalk.yellow(' You are NOT in a git repository!'));
+  askQuestions('?');
+}
+
+
+
+
+
+
+// simpleGit.log(['--grep=MOWV-69', '--author=Robbie Bardijn', '--no-merges'], function(bla, blabla) {
+//   console.log(blabla);
+// });
+
+// ncp.copy('some text BLA', function () {
+//   console.log('yeah');
+// });
